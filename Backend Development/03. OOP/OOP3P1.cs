86e23f542e7e2232e405252e53ea85d9d7a071b8
ismailTareq <164344079+ismailTareq@users.noreply.github.com>
2026-03-04 08:52:27 +0200

    Add files via upload

---
+++ Backend Development/03. OOP/OOP3P1.cs
@@ -0,0 +1,49 @@
+// Part1
+// Question 1
+// A)
+// departments literally can't exist without the university
+// hwa tt3ml m3aha w destroyed m3aha elmfrod fa howa kida child m3tamid 3ala el parent
+// deh kida composition
+
+// B)
+// Driver momkn yst5dim el car bs don't need to own it
+// ya3ni kida hya temporary relationship and both can exist without each other
+// deh kida association mfrod
+
+// C)
+// Dog is inherited from animal because it is a specific type of animal
+// deh kida inheritance
+
+// D)
+// player and team howa momken lw shilna team yb2a player
+// lsa exist 3adi momken join team tani kman
+// deh kida aggregation fl 28lab
+
+// E)
+// method mt3rafsh 8ir Logger kl mra 7yt3mlha call fa method 
+// kida m3tamid 3ala logger w
+// deh kida dependency
+
+// Question 2
+// A)
+// Protected fields is accessible within the class and its subclasses (inherited classes) only through inheritance
+// fa bl nisba lel child class yes can access it as long as child is extended from the parent class
+// but not through an object of the child class because it is not accessible outside the class and its subclasses
+
+// B)
+// Protected internal
+// accessible lw 3mlt inheritence from class (OR) in the same assembly
+
+// Private protected
+// accessible ONLY if you're both in the same assembly (AND) inheriting from the class 
+
+// C)
+// on class
+// it prevents anyone from inheriting from that class at all but can make create objects from it
+
+// on methods
+// it stops child classes from overriding that specific method any further
+
+// D)
+// lsa mgawb ah momkn hya btmna3 el inheritance bs 
+// el hwa ana momkn 2st5dim el class 3adi gdan bs mish 73mlo extantion 
