37f243247f9be1215cbac22ce6f753f20f1398a5
ismailTareq <164344079+ismailTareq@users.noreply.github.com>
2026-04-04 10:00:50 +0200

    Add files via upload

---
+++ Frontend Development /05React/10.form/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header.jsx';
+import StateLogin from './components/StateLogin.jsx';
+import Signup from './components/Signup.jsx'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <StateLogin />
+      </main>
+    </>
+  );
+}
+
+export default App;
